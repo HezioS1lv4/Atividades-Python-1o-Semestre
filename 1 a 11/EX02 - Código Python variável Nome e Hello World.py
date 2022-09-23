0f2d56ae84bec7a12937e226a848a36991ab5955
Hézio Silva <103318917+HezioS1lv4@users.noreply.github.com>
2022-09-23 11:32:21 -0300

    Add files via upload

---
+++ 1 a 11/EX02 - Código Python variável Nome e Hello World.py
@@ -0,0 +1,6 @@
+>>> nome= "Hézio Silva"
+>>> print(nome)
+Hézio Silva
+>>>
+>>> print("olá mundo, eu sou o", nome," e estou aqui para te conquistar.")
+olá mundo, eu sou o Hézio Silva  e estou aqui para te conquistar.
